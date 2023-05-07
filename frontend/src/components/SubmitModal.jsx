506666718aa830a57d35f7463eae83887aaca113
Samantha Finnie <samanthajglennon@gmail.com>
2023-05-07 16:15:18 -0700

    Created SubmitModal.jsx

---
+++ frontend/src/components/SubmitModal.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap'
+
+const SubmitModal = () => {
+    const [modal, setModal] = useState(false)
+    const toggle = () => setModal(!modal)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SubmitModal
