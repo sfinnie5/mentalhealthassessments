a2716c712d14f6104a741c867f9c0476d650b153
Samantha Finnie <samanthajglennon@gmail.com>
2023-05-24 11:23:55 -0700

    updated state variables for form submission

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,103 +1,96 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Phq9Form from './components/Phq9Form'
-import GAD7Form from './components/GAD7Form'
-import HomePage from './components/HomePage'
-import Table from './components/Table'
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Phq9Form from './components/Phq9Form';
+import GAD7Form from './components/GAD7Form';
+import HomePage from './components/HomePage';
+import Table from './components/Table';
+import { useState } from 'react';
 
-let nextId = 1
+let nextPhq9Id = 1;
+let nextGad7Id = 1;
 
 const App = () => {
-    const [submission, setSubmission] = useState([])
+  const [phq9Submissions, setPhq9Submissions] = useState([]);
+  const [gad7Submissions, setGad7Submissions] = useState([]);
 
-    const handleSubmit = (newAnswers) => {
-        const newSubmission = { id: nextId++, answers: newAnswers }
-        setSubmission([...submission, newSubmission])
-    }
+  const handlePhq9Submit = (newAnswers) => {
+    const newSubmission = { id: nextPhq9Id++, answers: newAnswers };
+    setPhq9Submissions([...phq9Submissions, newSubmission]);
+  };
 
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route exact path='/' element={<HomePage />} />
-                <Route
-                    path='/phq9form'
-                    element={
-                        <div>
-                            <Phq9Form onSubmit={handleSubmit} />
-                            <div className='assessment-list'>
-                                <h2 className='text-center'>
-                                    Prior Assessments:
-                                </h2>
-                                {submission.map((sub) => {
-                                    let total = 0
-                                    Object.values(sub.answers).forEach(
-                                        (answer) => {
-                                            total += answer
-                                        }
-                                    )
-                                    return (
-                                        <div key={sub.id}>
-                                            <p>Assessment ID: {sub.id}</p>
-                                            {Object.entries(sub.answers).map(
-                                                ([questionId, answer]) => (
-                                                    <p key={questionId}>
-                                                        Question {questionId}:{' '}
-                                                        {answer}
-                                                    </p>
-                                                )
-                                            )}
-                                            <p>Total: {total}</p>
-                                        </div>
-                                    )
-                                })}
-                                <Table />
-                            </div>
-                        </div>
-                    }
-                />
-                <Route
-                    path='/gad7form'
-                    element={
-                        <div>
-                            <GAD7Form onSubmit={handleSubmit} />
-                            <div className='assessment-list'>
-                                <h2 className='text-center'>
-                                    Prior Assessments:
-                                </h2>
-                                {submission.map((sub) => {
-                                    let total = 0
-                                    Object.values(sub.answers).forEach(
-                                        (answer) => {
-                                            total += answer
-                                        }
-                                    )
-                                    return (
-                                        <div key={sub.id}>
-                                            <p>Assessment ID: {sub.id}</p>
-                                            {Object.entries(sub.answers).map(
-                                                ([questionId, answer]) => (
-                                                    <p key={questionId}>
-                                                        Question {questionId}:{' '}
-                                                        {answer}
-                                                    </p>
-                                                )
-                                            )}
-                                            <p>Total: {total}</p>
-                                        </div>
-                                    )
-                                })}
-                                <Table />
-                            </div>
-                        </div>
-                    }
-                />
-            </Routes>
-            <Footer />
-        </Router>
-    )
-}
+  const handleGad7Submit = (newAnswers) => {
+    const newSubmission = { id: nextGad7Id++, answers: newAnswers };
+    setGad7Submissions([...gad7Submissions, newSubmission]);
+  };
 
-export default App
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route
+          path='/phq9form'
+          element={
+            <div>
+              <Phq9Form onSubmit={handlePhq9Submit} />
+              <div className='assessment-list'>
+                <h2 className='text-center'>Prior Assessments:</h2>
+                {phq9Submissions.map((sub) => {
+                  let total = 0;
+                  Object.values(sub.answers).forEach((answer) => {
+                    total += answer;
+                  });
+                  return (
+                    <div key={sub.id}>
+                      <p>Assessment ID: {sub.id}</p>
+                      {Object.entries(sub.answers).map(([questionId, answer]) => (
+                        <p key={questionId}>
+                          Question {questionId}: {answer}
+                        </p>
+                      ))}
+                      <p>Total: {total}</p>
+                    </div>
+                  );
+                })}
+                <Table />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path='/gad7form'
+          element={
+            <div>
+              <GAD7Form onSubmit={handleGad7Submit} />
+              <div className='assessment-list'>
+                <h2 className='text-center'>Prior Assessments:</h2>
+                {gad7Submissions.map((sub) => {
+                  let total = 0;
+                  Object.values(sub.answers).forEach((answer) => {
+                    total += answer;
+                  });
+                  return (
+                    <div key={sub.id}>
+                      <p>Assessment ID: {sub.id}</p>
+                      {Object.entries(sub.answers).map(([questionId, answer]) => (
+                        <p key={questionId}>
+                          Question {questionId}: {answer}
+                        </p>
+                      ))}
+                      <p>Total: {total}</p>
+                    </div>
+                  );
+                })}
+                <Table />
+              </div>
+            </div>
+          }
+        />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
